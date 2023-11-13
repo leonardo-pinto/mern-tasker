4ba55d6628f735916f64e4f7aa86ddaeb60fd584
leonardo-pinto <leonardoan.pinto@gmail.com>
2023-11-13 09:31:33 -0500

    feat: add NavigationBar component

---
+++ client/src/components/NavigationBar.jsx
@@ -0,0 +1,23 @@
+import mernLogo from "../assets/logo.png";
+import { Link } from "react-router-dom";
+
+function NavigationBar() {
+  return (
+    <>
+      <nav>
+        <img id="logo" src={mernLogo} className="logo react" alt="React logo" />
+        <ul>
+          <li>
+            <Link to="/login">Login</Link>
+          </li>
+          <li>
+            <Link to="/register">Register</Link>
+          </li>
+          <li>Logout</li>
+        </ul>
+      </nav>
+    </>
+  );
+}
+
+export default NavigationBar;
