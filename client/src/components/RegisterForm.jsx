4d72dbae21c277513675a972af0221fe29f01e80
leonardo-pinto <leonardoan.pinto@gmail.com>
2023-11-14 17:23:27 -0500

    feat: add userName state example

--- client/src/components/RegisterForm.jsx
+++ client/src/components/RegisterForm.jsx
@@ -1,31 +1,117 @@
+import { useState } from "react";
+
 function RegisterForm() {
-    return (
-        <>
-            <form style={{ border: '1px solid #ccc', padding: '20px', maxWidth: '300px', margin: 'auto' }}>
-                <label htmlFor="username">Username:</label>
-                <input type="text" id="username" name="username" required style={{ width: '100%', padding: '8px', margin: '8px 0', boxSizing: 'border-box' }} />
+  const [userName, setUserName] = useState("");
+  const [email, setEmail] = useState("");
+  // add onChange to the email input (e.g., onChange={handleEmail})
+  // create handleEmail function
+  // function handleEmail(e) { setEmail(e.target.value) }
 
-                <br />
+  const [password, setPassword] = useState("");
+  const [passwordConfirmation, setPasswordConfirmation] = useState("");
 
-                <label htmlFor="email">Email:</label>
-                <input type="email" id="email" name="email" required style={{ width: '100%', padding: '8px', margin: '8px 0', boxSizing: 'border-box' }} />
+  function handleUserName(e) {
+    setUserName(e.target.value);
+  }
 
-                <br />
+  function handleSubmit() {
+    alert(userName);
+    // call the backend with the form values
+  }
 
-                <label htmlFor="password">Password:</label>
-                <input type="password" id="password" name="password" required style={{ width: '100%', padding: '8px', margin: '8px 0', boxSizing: 'border-box' }} />
+  return (
+    <>
+      <form
+        style={{
+          border: "1px solid #ccc",
+          padding: "20px",
+          maxWidth: "300px",
+          margin: "auto",
+        }}
+      >
+        <label htmlFor="username">Username:</label>
+        <input
+          value={userName}
+          onChange={handleUserName}
+          type="text"
+          id="username"
+          name="username"
+          required
+          style={{
+            width: "100%",
+            padding: "8px",
+            margin: "8px 0",
+            boxSizing: "border-box",
+          }}
+        />
 
-                <br />
+        <br />
 
-                <label htmlFor="confirmPassword">Confirm Password:</label>
-                <input type="password" id="confirmPassword" name="confirmPassword" required style={{ width: '100%', padding: '8px', margin: '8px 0', boxSizing: 'border-box' }} />
+        <label htmlFor="email">Email:</label>
+        <input
+          type="email"
+          id="email"
+          name="email"
+          required
+          style={{
+            width: "100%",
+            padding: "8px",
+            margin: "8px 0",
+            boxSizing: "border-box",
+          }}
+        />
 
-                <br />
+        <br />
 
-                <input type="submit" value="Register" style={{ width: '100%', backgroundColor: '#4CAF50', color: 'white', padding: '10px', border: 'none', borderRadius: '5px', cursor: 'pointer' }} />
-            </form>
-        </>
-    );
+        <label htmlFor="password">Password:</label>
+        <input
+          type="password"
+          id="password"
+          name="password"
+          required
+          style={{
+            width: "100%",
+            padding: "8px",
+            margin: "8px 0",
+            boxSizing: "border-box",
+          }}
+        />
+
+        <br />
+
+        <label htmlFor="confirmPassword">Confirm Password:</label>
+        <input
+          type="password"
+          id="confirmPassword"
+          name="confirmPassword"
+          required
+          style={{
+            width: "100%",
+            padding: "8px",
+            margin: "8px 0",
+            boxSizing: "border-box",
+          }}
+        />
+
+        <br />
+
+        <input
+          type="submit"
+          value="Register"
+          onClick={handleSubmit}
+          style={{
+            width: "100%",
+            backgroundColor: "#4CAF50",
+            color: "white",
+            padding: "10px",
+            border: "none",
+            borderRadius: "5px",
+            cursor: "pointer",
+          }}
+        />
+      </form>
+    </>
+  );
 }
 
 export default RegisterForm;
