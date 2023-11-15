304b0390425f7d09d06c627773515be649fb903d
Pouya <poa.amd@gmail.com>
2023-11-15 14:32:33 -0500

    feat: Add usestate to LoginForm commponent

--- client/src/components/LoginForm.jsx
+++ client/src/components/LoginForm.jsx
@@ -1,21 +1,83 @@
+import { useState } from "react";
 function LoginForm() {
-    return (
-        <>
-            <form style={{ border: '1px solid #ccc', padding: '20px', maxWidth: '300px', margin: 'auto' }}>
-                <label htmlFor="username">Username:</label>
-                <input type="text" id="username" name="username" required style={{ width: '100%', padding: '8px', margin: '8px 0', boxSizing: 'border-box' }} />
+  const [userName, setUserName] = useState("");
+  const [password, setPassword] = useState("");
 
-                <br />
+  function handleUserName(e) {
+    const { value } = e.target;
+    setUserName(value);
+  }
+  function handlePassword(e) {
+    setPassword(e.target.value);
+  }
+  function handleSubmit(e) {
+    e.preventDefault();
+    alert(`Username: ${userName}, Password: ${password}`);
+  }
+  return (
+    <>
+      <form
+        style={{
+          border: "1px solid #ccc",
+          padding: "20px",
+          maxWidth: "300px",
+          margin: "auto",
+        }}
+      >
+        <h1>Login</h1>
+        <label htmlFor="username">Username:</label>
+        <input
+          value={userName}
+          onChange={handleUserName}
+          type="text"
+          id="username"
+          name="username"
+          required
+          style={{
+            width: "100%",
+            padding: "8px",
+            margin: "8px 0",
+            boxSizing: "border-box",
+          }}
+        />
 
-                <label htmlFor="password">Password:</label>
-                <input type="password" id="password" name="password" required style={{ width: '100%', padding: '8px', margin: '8px 0', boxSizing: 'border-box' }} />
+        <br />
 
-                <br />
+        <label htmlFor="password">Password:</label>
+        <input
+          value={password}
+          onChange={handlePassword}
+          type="password"
+          id="password"
+          name="password"
+          required
+          style={{
+            width: "100%",
+            padding: "8px",
+            margin: "8px 0",
+            boxSizing: "border-box",
+          }}
+        />
 
-                <input type="submit" value="Login" style={{ width: '100%', backgroundColor: '#4CAF50', color: 'white', padding: '10px', border: 'none', borderRadius: '5px', cursor: 'pointer' }} />
-            </form>
-        </>
-    );
+        <br />
+
+        <input
+          onClick={handleSubmit}
+          type="submit"
+          value="Login"
+          style={{
+            width: "100%",
+            backgroundColor: "#4CAF50",
+            color: "white",
+            padding: "10px",
+            border: "none",
+            borderRadius: "5px",
+            cursor: "pointer",
+          }}
+        />
+      </form>
+    </>
+  );
 }
 
 export default LoginForm;
