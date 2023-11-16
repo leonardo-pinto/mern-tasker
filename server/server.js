40a9e6d0b0c26a35ccf87e8e857444e5065861cf
leonardo-pinto <leonardoan.pinto@gmail.com>
2023-11-15 20:46:37 -0500

    fix: update config import and properties for port

--- server/server.js
+++ server/server.js
@@ -1,12 +1,12 @@
 const mongoose = require("mongoose");
 const app = require("./app");
-const config = require("./config");
+const { config } = require("./config");
 
 (async () => {
   try {
     await mongoose.connect(config.mongoUri);
-    app.listen(config.PORT, () =>
-      console.log(`Server running on port ${config.PORT}`)
+    app.listen(config.port, () =>
+      console.log(`Server running on port ${config.port}`)
     );
   } catch (error) {
     console.error(
