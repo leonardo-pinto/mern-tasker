2f46dc4aaf148e705de61bda80dd11fc80be3f7a
leonardo-pinto <leonardoan.pinto@gmail.com>
2023-11-16 09:12:04 -0500

    feat: add update task route

--- server/routes/taskRoutes.js
+++ server/routes/taskRoutes.js
@@ -1,7 +1,16 @@
 const express = require("express");
 const router = express.Router();
-const { create, getAll, deleteTask } = require("../controllers/taskController");
+const {
+  create,
+  getAll,
+  deleteTask,
+  update,
+} = require("../controllers/taskController");
 
-router.post("/", create).get("/", getAll).delete("/:id", deleteTask);
+router
+  .post("/", create)
+  .get("/", getAll)
+  .delete("/:id", deleteTask)
+  .put("/:id", update);
 
 module.exports = router;
