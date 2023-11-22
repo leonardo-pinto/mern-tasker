cc5cf9fe65280efb4cda25fb9437203283f107dd
leonardo-pinto <leonardoan.pinto@gmail.com>
2023-11-22 14:16:54 -0500

    fix: set login and register as strings

--- client/src/api/authApi.js
+++ client/src/api/authApi.js
@@ -3,11 +3,11 @@ import httpClient from "./httpClient";
 const AUTH_ROUTE = "/auth";
 
 async function login(loginData) {
-  return (await httpClient.post(`${AUTH_ROUTE}/${login}`, loginData)).data;
+  return (await httpClient.post(`${AUTH_ROUTE}/login`, loginData)).data;
 }
 
 async function register(registerData) {
-  return (await httpClient.post(`${AUTH_ROUTE}/${register}`, registerData)).data;
+  return (await httpClient.post(`${AUTH_ROUTE}/register`, registerData)).data;
 }
 
 export { login, register };
