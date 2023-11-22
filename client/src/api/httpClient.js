1d7ebe242cbdd61d439646426ac9746676fa11b0
leonardo-pinto <leonardoan.pinto@gmail.com>
2023-11-22 15:20:06 -0500

    feat: add request and response intereceptors to axios

--- client/src/api/httpClient.js
+++ client/src/api/httpClient.js
@@ -1,4 +1,7 @@
 import axios from "axios";
+import { useNavigate } from "react-router-dom";
+
+const navigate = useNavigate();
 
 const httpClient = axios.create({
   baseURL: "http://localhost:3000/api",
@@ -6,5 +9,33 @@ const httpClient = axios.create({
     "Content-Type": "application/json",
   },
 });
+
+const getAuthAccessToken = () => localStorage.getItem("accessToken");
+
+const authInterceptor = (config) => {
+  config.headers["Authorization"] = `Bearer ${getAuthAccessToken()}`;
+  return config;
+};
+
+const responseInterceptor = (response) => response;
+
+const errorInterceptor = (error) => {
+  if (!error.response) {
+    console.error("Server is down!");
+  }
+
+  if (error?.response?.status === 401) {
+    clearLocalStorage();
+    navigate("/login", { replace: true });
+  }
+
+  if (error?.response?.status === 500) {
+    console.error("An unexpected server error occurred!");
+  }
+  return Promise.reject(error);
+};
+
+httpClient.interceptors.request.use(authInterceptor);
+httpClient.interceptors.response.use(responseInterceptor, errorInterceptor);
 
 export default httpClient;
