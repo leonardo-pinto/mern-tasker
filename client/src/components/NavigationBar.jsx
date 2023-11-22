069a5e44345761e06ec75e35af39e37dcfcfc068
leonardo-pinto <leonardoan.pinto@gmail.com>
2023-11-22 14:33:20 -0500

    feat: add logout feature

--- client/src/components/NavigationBar.jsx
+++ client/src/components/NavigationBar.jsx
@@ -1,7 +1,15 @@
 import mernLogo from "../assets/logo.png";
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
+import { clearLocalStorage } from "../utils";
 
 function NavigationBar() {
+  const navigate = useNavigate();
+
+  function logout() {
+    clearLocalStorage();
+    navigate("/login", { replace: true });
+  }
+
   return (
     <>
       <nav>
@@ -16,7 +24,9 @@ function NavigationBar() {
           <li>
             <Link to="/register">Register</Link>
           </li>
-          <li>Logout</li>
+          <li>
+            <button onClick={logout}>Logout</button>
+          </li>
         </ul>
       </nav>
     </>
