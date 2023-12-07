208ba16bddb639ec245c880f4e3260ce8d162b39
leonardo-pinto <leonardoan.pinto@gmail.com>
2023-12-07 10:09:32 -0500

    refactor: add env variables

--- server/config/index.js
+++ server/config/index.js
@@ -1,9 +1,9 @@
+require("dotenv").config();
+
 const config = {
   port: process.env.PORT || 3000,
   jwtSecret: process.env.JWT_SECRET || "YOUR_secret_key",
-  mongoUri:
-    process.env.MONGODB_URI ||
-    "mongodb+srv://atlasAdmin:mpVxAnBxsONOY9dY@cluster0.vcv1smx.mongodb.net/?retryWrites=true&w=majority",
+  mongoUri: process.env.MONGODB_URI || "mongodb://localhost:27017",
 };
 
 module.exports = { config };
