e51f17a9adab0ccd91607d14f7e3d88a59e867fd
leonardo-pinto <leonardoan.pinto@gmail.com>
2023-12-07 08:32:19 -0500

    add validation objects for react hook form

---
+++ client/src/utils/validation/index.js
@@ -0,0 +1,49 @@
+export const loginHookFormValidation = {
+  username: {
+    required: "Username is required",
+    minLength: {
+      value: 6,
+      message: "Username must have at least 6 characters",
+    },
+  },
+  password: {
+    required: "Password is required",
+    minLength: {
+      value: 6,
+      message: "Password must have at least 6 characters",
+    },
+  },
+};
+
+export const registerHookFormValidation = {
+  ...loginHookFormValidation,
+  email: {
+    require: "Email is required",
+    pattern: {
+      value:
+        /^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/,
+      message: "Invalid email address",
+    },
+  },
+};
+
+export const createTaskHookFormValidation = {
+  title: {
+    required: "Title is required",
+  },
+  date: {
+    required: "Date is required",
+  },
+};
+
+export const updateTaskHookFormValidation = {
+  title: {
+    required: "Title is required",
+  },
+  date: {
+    required: "Date is required",
+  },
+  status: {
+    required: "Status is required",
+  },
+};
