f08a92f6a19ef01410fa0292597a006c37f0bda7
leonardo-pinto <leonardoan.pinto@gmail.com>
2023-12-07 09:01:02 -0500

    chore: update validation messages

--- client/src/utils/validation/index.js
+++ client/src/utils/validation/index.js
@@ -18,11 +18,11 @@ export const loginHookFormValidation = {
 export const registerHookFormValidation = {
   ...loginHookFormValidation,
   email: {
-    require: "Email is required",
+    required: "Email is required",
     pattern: {
       value:
         /^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/,
-      message: "Invalid email address",
+      message: "Invalid email format",
     },
   },
 };
